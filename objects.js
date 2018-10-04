905ed656c35a6d6187d8fa46984f138eb4aa5862
Henry Brooke-Dean <henrybrookedean@hotmail.com>
2018-10-04 14:57:40 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = {"My Bloody Valentine": "song1",
   "Slowdive": "song2",
   "Phil Ochs": "song3"};
   
-function updatePlaylist(thePlaylist, artistName, songTitle) {
+function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, thePlaylist, { [artistName]: songTitle});
 }
 updatePlaylist(playlist, "artist4", "song4");  
